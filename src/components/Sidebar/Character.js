ba0c6c88c721c7681e841d80ea5c6745d87ce1e6
Andrea <Andrea.Lunghi3@gmail.com>
2022-01-25 18:24:16 +0100

    New Character Image

--- src/components/Sidebar/Character.js
+++ src/components/Sidebar/Character.js
@@ -1,23 +1,25 @@
 import React from "react";
+import CharacterImage from "./CharacterImage";
 
-const Character = ({ name, tag, image }) => {
+const Character = ({ character }) => {
   return (
     <div className="character h-14 w-full px-5 my-2 flex items-center">
-      <div className="character-image h-14 w-14 p-1 border-2 border-solid border-primary rounded-full">
-        <img className="h-full w-full rounded-full" src={image} alt="" />
-      </div>
+      <CharacterImage image={character.image} level={2} exp={25} />  {/* TODO Aggiungere formula per calcolo EXP */}
       <div className="character-info ml-2">
-        <h4 className="character-name">{name}</h4>
-        <h5 className="character-tag">{tag}</h5>
+        <h4 className="character-name">{character.name}</h4>
+        <h5 className="character-tag">{character.tag}</h5>
       </div>
     </div>
   );
 };
 
 Character.defaultProps = {
-  name: "Character Name",
-  tag: "Character Tag",
-  image: "",
+  character: {
+    name: "Character Name",
+    tag: "Character Tag",
+    image: "",
+    exp: 0
+  },
 };
 
 export default Character;
