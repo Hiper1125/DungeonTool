a128722d6765d5ed8e67a808bb3fe3a501f3f3b1
Andrea <Andrea.Lunghi3@gmail.com>
2022-02-06 15:22:30 +0100

    Loading bar fix

--- src/components/Loading/LoadingBar.jsx
+++ src/components/Loading/LoadingBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LoadingBar = () => {
   return (
     <div className="loading-bar h-3 w-64 absolute bottom-10 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray rounded-full overflow-hidden">
-      <div className="progress h-full w-0 bg-primary rounded-r-full transition-all"></div>
+      <div className="progress h-full bg-primary rounded-r-full transition-all" style={{width: 0}}></div>
     </div>
   );
 };
